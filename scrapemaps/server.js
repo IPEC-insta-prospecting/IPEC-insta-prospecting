58c55dc2ad4be668440e45027dc87def0f8b6fb0
lunquin222 <vzvdvzvdvzvd@gmail.com>
2024-12-26 20:26:33 -0300

    Update server.js

--- scrapemaps/server.js
+++ scrapemaps/server.js
@@ -42,7 +42,6 @@ app.post('/scrape', async (req, res) => {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36');
     await page.setExtraHTTPHeaders({ 'Accept-Language': 'en-US,en;q=0.9' });
   } catch (err) {
-    console.error("Erro ao configurar cabeçalhos:", err);
   }
 
   const urlDePesquisa = "https://www.google.com.br/maps";
